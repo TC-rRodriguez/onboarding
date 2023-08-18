7e31d91f2ead7d0fa6893baea83299dfa6bfa862
Jaime Bautista <jbautista@tigerconnect.com>
2023-08-18 15:28:54 -0600

    feat: Add SOLID Design Principles project

---
+++ C#/Onboarding/SOLID Design Principles/1-SingleResponsability/Program.cs
@@ -0,0 +1,8 @@
+﻿using SingleResponsability;
+
+StudentRepository studentRepository = new();
+studentRepository.Export();
+Console.WriteLine("Process completed!");
+
+Console.WriteLine("Press any key to finish...");
+Console.ReadKey();
---
+++ C#/Onboarding/SOLID Design Principles/4-InterfaceSegregation/Program.cs
@@ -0,0 +1,6 @@
+﻿using InterfaceSegregation;
+
+new Developer().Develop();
+
+Console.WriteLine("Press any key to finish...");
+Console.ReadKey();
