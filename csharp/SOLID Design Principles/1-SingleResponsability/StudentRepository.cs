95b59353e7ec55a5ed6414ad5ef8ca6df4fc0ff9
Alejandro Rodriguez <rrodriguez@tigerconnect.com>
2024-12-11 14:35:26 -0600

    feat: enforce SRP by refactoring code
    - Refactor code to adhere to the Single Responsibility Principle (SRP)
    - Create `StudentFormatter` class to handle CSV string formatting for exported data
    - Create `SaveToCsv` class responsible for writing the output file
    - Create `StudentExporter` class to manage all export data processing
    - Modify `StudentRepository` to handle only data operations, delegating file storage to `StudentExporter`
    - Add XML documentation for all classes and methods to improve maintainability
    
    Updated project to .Net 9 in order to take advantage of new features.

--- csharp/SOLID Design Principles/1-SingleResponsability/StudentRepository.cs
+++ csharp/SOLID Design Principles/1-SingleResponsability/StudentRepository.cs
@@ -1,40 +1,57 @@
-using System.Text;
+namespace SingleResponsability;
 
-namespace SingleResponsability
+/// <summary>
+/// Repository class for managing Student entities.
+/// </summary>
+public class StudentRepository
 {
-    public class StudentRepository
+    private static FakeStorage<Student> storage;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StudentRepository"/> class.
+    /// </summary>
+    public StudentRepository()
     {
-        private static FakeStorage<Student> storage;
+        storage = new FakeStorage<Student>();
+        InitData();
+    }
 
-        public StudentRepository()
-        {
-            storage = new();
-            InitData();
-        }
+    /// <summary>
+    /// Initializes the repository with sample data.
+    /// </summary>
+    private static void InitData()
+    {
+        storage.Add(new Student(1, "Pepito Pérez", [3, 4.5]));
+        storage.Add(new Student(2, "Mariana Lopera", [4, 5]));
+        storage.Add(new Student(3, "José Molina", [2, 3]));
+    }
 
-        private void InitData()
-        {
-            storage.Add(new Student(1, "Pepito Pérez", new List<double>() { 3, 4.5 }));
-            storage.Add(new Student(2, "Mariana Lopera", new List<double>() { 4, 5 }));
-            storage.Add(new Student(3, "José Molina", new List<double>() { 2, 3 }));
-        }
+    /// <summary>
+    /// Adds a new student to the repository.
+    /// </summary>
+    /// <param name="student">The student to add.</param>
+    /// <returns>The added student.</returns>
+    public Student Add(Student student)
+    {
+        return storage.Add(student);
+    }
+    
+    /// <summary>
+    /// Removes a student from the repository.
+    /// </summary>
+    /// <param name="student">The student to remove.</param>
+    /// <returns>The removed student.</returns>
+    public Student Remove(Student student)
+    {
+        return storage.Remove(student);
+    }
 
-        public IEnumerable<Student> GetAll() 
-        {
-            return storage.GetAll();
-        }
-
-        public void Export() 
-        {
-            IEnumerable<Student> students = this.GetAll();
-            string csv = String.Join(",", students.Select(x => x.ToString()).ToArray());
-            System.Text.StringBuilder sb = new System.Text.StringBuilder();
-            sb.AppendLine("Id;Fullname;Grades");
-            foreach (var item in students)
-            {
-                sb.AppendLine($"{item.Id};{item.Fullname};{string.Join("|", item.Grades)}");
-            }
-            System.IO.File.WriteAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Students.csv"), sb.ToString(), Encoding.Unicode);
-        }
+    /// <summary>
+    /// Gets all students from the repository.
+    /// </summary>
+    /// <returns>An enumerable collection of students.</returns>
+    public IEnumerable<Student> GetAll() 
+    {
+        return storage.GetAll();
     }
 }
